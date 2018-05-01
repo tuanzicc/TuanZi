6c04aa5343ed5818f132c030a85972e34c5359b5
TuanZi <tan@tnzi.cc>
2018-05-01 02:08:36 -0400

    11

---
+++ TuanZi.AspNetCore/Mvc/Extenstions/ModelStateExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuanZi.AspNetCore.Mvc
+{
+    public static class ModelStateExtensions
+    {
+        public static string FirstError(this ModelStateDictionary modelState)
+        {
+            var errorText = string.Empty;
+            foreach (var state in modelState.Values)
+            {
+                if (state != null && state.Errors.Count > 0)
+                {
+                    var error = state.Errors[0];
+                    errorText = error.ErrorMessage;
+                    if (errorText.IsNullOrEmpty() && error.Exception != null)
+                        errorText = state.Errors[0].Exception.Message;
+                    break;
+                }
+            }
+
+
+            return errorText;
+        }
+
+        public static List<string> Errors(this ModelStateDictionary modelState)
+        {
+            var errors = new List<string>();
+            foreach (var state in modelState.Values)
+            {
+                if (state != null && state.Errors.Count > 0)
+                {
+                    var error = state.Errors[0];
+                    var errorText = error.ErrorMessage;
+                    if (errorText.IsNullOrEmpty() && error.Exception != null)
+                        errorText = state.Errors[0].Exception.Message;
+                    errors.Add(errorText);
+                }
+            }
+
+
+            return errors;
+        }
+    }
+}
