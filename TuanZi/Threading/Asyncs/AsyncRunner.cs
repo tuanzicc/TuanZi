d8d8dbfc324303c499fb4df46bc8289d4735a0f5
TuanZi <tan@tnzi.cc>
2018-06-23 12:32:54 -0400

    a

---
+++ TuanZi/Threading/Asyncs/AsyncRunner.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TuanZi.Threading.Asyncs
+{
+
+    public static class AsyncRunner
+    {
+
+        /// <summary>
+        /// Executes an async Task method which has a void return value synchronously
+        /// </summary>
+        /// <param name="func">Task method to execute</param>
+        public static void RunSync(Func<Task> func)
+        {
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            synch.Post(async _ =>
+            {
+                try
+                {
+                    await func();
+                }
+                catch (Exception e)
+                {
+                    synch.InnerException = e;
+                    throw;
+                }
+                finally
+                {
+                    synch.EndMessageLoop();
+                }
+            }, null);
+            synch.BeginMessageLoop();
+
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+        }
+
+        /// <summary>
+        /// Executes an async Task method which has a TResult return type synchronously
+        /// </summary>
+        /// <typeparam name="TResult">Return Type</typeparam>
+        /// <param name="func">Task method to execute</param>
+        /// <returns></returns>
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            TResult ret = default(TResult);
+            synch.Post(async _ =>
+            {
+                try
+                {
+                    ret = await func();
+                }
+                catch (Exception e)
+                {
+                    synch.InnerException = e;
+                    throw;
+                }
+                finally
+                {
+                    synch.EndMessageLoop();
+                }
+            }, null);
+            synch.BeginMessageLoop();
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+            return ret;
+        }
+
+
+
+
+        private class ExclusiveSynchronizationContext : SynchronizationContext
+        {
+            private bool _done;
+            readonly AutoResetEvent _workItemsWaiting = new AutoResetEvent(false);
+            readonly Queue<Tuple<SendOrPostCallback, object>> _items = new Queue<Tuple<SendOrPostCallback, object>>();
+
+            public Exception InnerException { get; set; }
+
+            public override void Send(SendOrPostCallback d, object state)
+            {
+                throw new NotSupportedException("We cannot send to the same thread");
+            }
+
+            public override void Post(SendOrPostCallback d, object state)
+            {
+                lock (_items)
+                {
+                    _items.Enqueue(Tuple.Create(d, state));
+                }
+                _workItemsWaiting.Set();
+            }
+
+            public void EndMessageLoop()
+            {
+                Post(_ => _done = true, null);
+            }
+
+            public void BeginMessageLoop()
+            {
+                while (!_done)
+                {
+                    Tuple<SendOrPostCallback, object> task = null;
+                    lock (_items)
+                    {
+                        if (_items.Count > 0)
+                        {
+                            task = _items.Dequeue();
+                        }
+                    }
+                    if (task != null)
+                    {
+                        task.Item1(task.Item2);
+                        if (InnerException != null) // the method threw an exeption
+                        {
+                            throw new AggregateException("AsyncHelpers.Run method threw an exception.", InnerException);
+                        }
+                    }
+                    else
+                    {
+                        _workItemsWaiting.WaitOne();
+                    }
+                }
+            }
+
+            public override SynchronizationContext CreateCopy()
+            {
+                return this;
+            }
+        }
+
+    }
+
+
+}
