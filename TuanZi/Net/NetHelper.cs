297110932a0c5816a3c0b768f7d18ea57b87a886
TuanZi <tan@tnzi.cc>
2018-06-25 16:51:51 -0400

    a

--- TuanZi/Net/NetHelper.cs
+++ TuanZi/Net/NetHelper.cs
@@ -1,13 +1,42 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
-
+using System.Threading.Tasks;
 
 namespace TuanZi.Net
 {
     public static class NetHelper
     {
-        public static bool Ping(string ip)
+        public static string GetLocalIPAddress()
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                var endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
+
+        public static List<string> GetLocalIPAddresses()
+        {
+            var list = new List<string>();
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    list.Add(ip.ToString());
+                }
+            }
+            return list;
+        }
+
+        public static bool Ping(string hostNameOrAddress, bool error = false)
         {
             try
             {
@@ -16,12 +45,54 @@ namespace TuanZi.Net
                 string data = "Test Data";
                 byte[] buffer = Encoding.ASCII.GetBytes(data);
                 int timeout = 1000;
-                PingReply reply = ping.Send(ip, timeout, buffer, options);
+                PingReply reply = ping.Send(hostNameOrAddress, timeout, buffer, options);
                 return reply != null && reply.Status == IPStatus.Success;
             }
-            catch (PingException)
+            catch (PingException e)
             {
+                if (error)
+                    throw e;
                 return false;
+            }
+        }
+
+        public static async Task<bool> PingAsync(string hostNameOrAddress, bool error = false)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingOptions options = new PingOptions { DontFragment = true };
+                string data = "Test Data";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 1000;
+                PingReply reply = await ping.SendPingAsync(hostNameOrAddress, timeout, buffer, options);
+                return reply != null && reply.Status == IPStatus.Success;
+            }
+            catch (PingException e)
+            {
+                if (error)
+                    throw e;
+                return false;
+            }
+        }
+
+        public static async Task<PingReply> PingReplyAsync(string hostNameOrAddress, bool error = false)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingOptions options = new PingOptions { DontFragment = true };
+                string data = "Test Data";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 1000;
+                PingReply reply = await ping.SendPingAsync(hostNameOrAddress, timeout, buffer, options);
+                return reply;
+            }
+            catch (PingException e)
+            {
+                if (error)
+                    throw e;
+                return null;
             }
         }
 
