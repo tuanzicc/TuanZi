ae72ec7136786e1f539b19f1470bb50e8aa47e19
TuanZi <tan@tnzi.cc>
2018-08-23 15:29:54 -0400

    s

--- TuanZi/Collections/EnumerableExtensions.cs
+++ TuanZi/Collections/EnumerableExtensions.cs
@@ -116,5 +116,15 @@ namespace TuanZi.Collections
             }
         }
 
+        public static T PickRandom<T>(this IEnumerable<T> source)
+        {
+            return source.PickRandom(1).Single();
+        }
+
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Shuffle().Take(count);
+        }
+
     }
 }
